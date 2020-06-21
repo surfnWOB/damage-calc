surfnWOB/damage-calc

commit 3bb6127c9d3cd28a19b9bcb497dd74b787c7686d

Pokemon SS Isle of Armor DLC (#397)
Co-authored-by: Kris Johnson <11083252+KrisXV@users.noreply.github.com>

calc/src/data/moves.ts
@@ -2891,7 +2891,24 @@ const SS_PATCH: {[name: string]: DeepPartial<MoveData>} = {
     zp: 120,
     maxPower: 110,
   },
+  'Burning Jealousy': {
+    bp: 70,
+    type: 'Dragon',
+    target: 'allAdjacentFoes',
+    category: 'Special',
+    secondaries: true,
+  },
   'Clangorous Soul': {bp: 0, type: 'Dragon', isSound: true},
+  'Coaching': {
+    bp: 0,
+    type: 'Fighting',
+    category: 'Status',
+  },
+  'Corrosive Gas': {
+    bp: 0,
+    type: 'Poison',
+    category: 'Status',
+  },
   'Court Change': {bp: 0, type: 'Normal'},
   Decorate: {bp: 0, type: 'Fairy'},
   'Dragon Darts': {
@@ -2910,6 +2927,14 @@ const SS_PATCH: {[name: string]: DeepPartial<MoveData>} = {
     zp: 160,
     maxPower: 130,
   },
+  'Dual Wingbeat': {
+    bp: 40,
+    type: 'Flying',
+    category: 'Physical',
+    makesContact: true,
+    multihit: 2,
+    maxPower: 130,
+  },
   'Dynamax Cannon': {
     bp: 100,
     type: 'Dragon',
@@ -2923,6 +2948,11 @@ const SS_PATCH: {[name: string]: DeepPartial<MoveData>} = {
     category: 'Special',
     zp: 200,
     maxPower: 150,
+  },
+  'Expanding Force': {
+    bp: 80,
+    type: 'Psychic',
+    category: 'Special',
   },
   'False Surrender': {
     bp: 80,
@@ -2941,6 +2971,11 @@ const SS_PATCH: {[name: string]: DeepPartial<MoveData>} = {
     zp: 160,
     maxPower: 130,
   },
+  'Flip Turn': {
+    bp: 60,
+    type: 'Water',
+    category: 'Physical',
+  },
   'G-Max Befuddle': {
     bp: 10,
     type: 'Bug',
@@ -2951,6 +2986,13 @@ const SS_PATCH: {[name: string]: DeepPartial<MoveData>} = {
   'G-Max Centiferno': {
     bp: 10,
     type: 'Fire',
+    category: 'Physical',
+    isMax: true,
+    maxPower: 1,
+  },
+  'G-Max Cannonade': {
+    bp: 10,
+    type: 'Water',
     category: 'Physical',
     isMax: true,
     maxPower: 1,
@@ -2972,6 +3014,20 @@ const SS_PATCH: {[name: string]: DeepPartial<MoveData>} = {
   'G-Max Depletion': {
     bp: 10,
     type: 'Dragon',
+    category: 'Physical',
+    isMax: true,
+    maxPower: 1,
+  },
+  'G-Max Drum Solo': {
+    bp: 10,
+    type: 'Grass',
+    category: 'Physical',
+    isMax: true,
+    maxPower: 1,
+  },
+  'G-Max Fire Ball': {
+    bp: 10,
+    type: 'Fire',
     category: 'Physical',
     isMax: true,
     maxPower: 1,
@@ -3004,6 +3060,13 @@ const SS_PATCH: {[name: string]: DeepPartial<MoveData>} = {
     isMax: true,
     maxPower: 1,
   },
+  'G-Max Hydrosnipe': {
+    bp: 10,
+    type: 'Water',
+    category: 'Physical',
+    isMax: true,
+    maxPower: 1,
+  },
   'G-Max Malodor': {
     bp: 10,
     type: 'Poison',
@@ -3014,6 +3077,20 @@ const SS_PATCH: {[name: string]: DeepPartial<MoveData>} = {
   'G-Max Meltdown': {
     bp: 10,
     type: 'Steel',
+    category: 'Physical',
+    isMax: true,
+    maxPower: 1,
+  },
+  'G-Max One Blow': {
+    bp: 10,
+    type: 'Dark',
+    category: 'Physical',
+    isMax: true,
+    maxPower: 1,
+  },
+  'G-Max Rapid Flow': {
+    bp: 10,
+    type: 'Water',
     category: 'Physical',
     isMax: true,
     maxPower: 1,
@@ -3095,6 +3172,13 @@ const SS_PATCH: {[name: string]: DeepPartial<MoveData>} = {
     isMax: true,
     maxPower: 1,
   },
+  'G-Max Vine Lash': {
+    bp: 10,
+    type: 'Grass',
+    category: 'Physical',
+    isMax: true,
+    maxPower: 1,
+  },
   'G-Max Volcalith': {
     bp: 10,
     type: 'Rock',
@@ -3123,6 +3207,14 @@ const SS_PATCH: {[name: string]: DeepPartial<MoveData>} = {
     isMax: true,
     maxPower: 1,
   },
+  'Grassy Glide': {
+    bp: 70,
+    type: 'Grass',
+    category: 'Physical',
+    makesContact: true,
+    zp: 160,
+    maxPower: 130,
+  },
   'Grav Apple': {
     bp: 80,
     type: 'Grass',
@@ -3139,6 +3231,17 @@ const SS_PATCH: {[name: string]: DeepPartial<MoveData>} = {
     category: 'Physical',
     zp: 160,
     maxPower: 130,
+  },
+  'Jungle Healing': {
+    bp: 0,
+    type: 'Grass',
+    category: 'Status',
+  },
+  'Lash Out': {
+    bp: 75,
+    type: 'Dark',
+    makesContact: true,
+    category: 'Physical',
   },
   'Life Dew': {bp: 0, type: 'Water'},
   'Magic Powder': {bp: 0, type: 'Psychic'},
@@ -3276,6 +3379,16 @@ const SS_PATCH: {[name: string]: DeepPartial<MoveData>} = {
     zp: 200,
     maxPower: 100,
   },
+  'Meteor Beam': {
+    bp: 120,
+    type: 'Rock',
+    category: 'Special',
+  },
+  'Misty Explosion': {
+    bp: 100,
+    type: 'Fairy',
+    category: 'Special',
+  },
   'No Retreat': {bp: 0, type: 'Fighting'},
   Obstruct: {bp: 0, type: 'Dark', priority: 4},
   Octolock: {bp: 0, type: 'Fighting'},
@@ -3288,6 +3401,11 @@ const SS_PATCH: {[name: string]: DeepPartial<MoveData>} = {
     zp: 160,
     maxPower: 130,
   },
+  Poltergeist: {
+    bp: 80,
+    type: 'Dark',
+    category: 'Physical',
+  },
   'Pyro Ball': {
     bp: 120,
     type: 'Fire',
@@ -3296,6 +3414,19 @@ const SS_PATCH: {[name: string]: DeepPartial<MoveData>} = {
     isBullet: true,
     zp: 190,
     maxPower: 140,
+  },
+  'Rising Voltage': {
+    bp: 70,
+    type: 'Electric',
+    category: 'Special',
+  },
+  'Scale Shot': {
+    bp: 25,
+    type: 'Dragon',
+    category: 'Physical',
+    multihit: 5,
+    zp: 140,
+    maxPower: 130,
   },
   'Snap Trap': {
     bp: 35,
@@ -3311,6 +3442,19 @@ const SS_PATCH: {[name: string]: DeepPartial<MoveData>} = {
     category: 'Special',
     zp: 160,
     maxPower: 130,
+  },
+  'Scorching Sands': {
+    bp: 70,
+    type: 'Ground',
+    category: 'Special',
+    secondaries: true,
+  },
+  'Skitter Smack': {
+    bp: 70,
+    type: 'Bug',
+    category: 'Physical',
+    makesContact: true,
+    secondaries: true,
   },
   'Spirit Break': {
     bp: 75,
@@ -3329,6 +3473,11 @@ const SS_PATCH: {[name: string]: DeepPartial<MoveData>} = {
     zp: 200,
     maxPower: 140,
   },
+  'Steel Roller': {
+    bp: 130,
+    type: 'Steel',
+    category: 'Physical',
+  },
   'Strange Steam': {
     bp: 90,
     type: 'Fairy',
@@ -3336,6 +3485,37 @@ const SS_PATCH: {[name: string]: DeepPartial<MoveData>} = {
     secondaries: true,
     zp: 175,
     maxPower: 130,
+  },
+  'Surging Strikes': {
+    bp: 25,
+    type: 'Water',
+    category: 'Physical',
+    makesContact: true,
+    willCrit: true,
+    multihit: 3,
+  },
+  'Terrain Pulse': {
+    bp: 50,
+    type: 'Normal',
+    category: 'Special',
+    isPulse: true,
+    zp: 160,
+    maxPower: 130,
+  },
+  'Triple Axel': {
+    bp: 20,
+    type: 'Ice',
+    category: 'Physical',
+    makesContact: true,
+    multihit: 3,
+    maxPower: 130,
+  },
+  'Wicked Blow': {
+    bp: 80,
+    type: 'Dark',
+    category: 'Physical',
+    makesContact: true,
+    willCrit: true,
   },
   'Stuff Cheeks': {bp: 0, type: 'Normal'},
   'Tar Shot': {bp: 0, type: 'Rock'},

calc/src/data/species.ts
@@ -7451,6 +7451,7 @@ const SS_PATCH: {[name: string]: DeepPartial<SpeciesData>} = {
   'Aegislash-Blade': {bs: {at: 140, sa: 140}},
   'Aegislash-Both': {bs: {at: 140, df: 140, sa: 140, sd: 140}},
   'Aegislash-Shield': {bs: {df: 140, sd: 140}},
+  Blastoise: {otherFormes: ['Blastoise-Gmax', 'Blastoise-Mega']},
   Butterfree: {otherFormes: ['Butterfree-Gmax']},
   Charizard: {otherFormes: ['Charizard-Gmax', 'Charizard-Mega-X', 'Charizard-Mega-Y']},
   Corsola: {otherFormes: ['Corsola-Galar']},
@@ -7479,13 +7480,16 @@ const SS_PATCH: {[name: string]: DeepPartial<SpeciesData>} = {
       'Pikachu-Partner',
       'Pikachu-Sinnoh',
       'Pikachu-Unova',
+      'Pikachu-World',
     ],
   },
   Ponyta: {otherFormes: ['Ponyta-Galar']},
   Rapidash: {otherFormes: ['Rapidash-Galar']},
+  Slowbro: {otherFormes: ['Slowbro-Galar', 'Slowbro-Mega']},
   Slowpoke: {otherFormes: ['Slowpoke-Galar']},
   Snorlax: {otherFormes: ['Snorlax-Gmax']},
   Stunfisk: {otherFormes: ['Stunfisk-Galar']},
+  Venusaur: {otherFormes: ['Venusaur-Gmax', 'Venusaur-Mega']},
   Weezing: {otherFormes: ['Weezing-Galar']},
   Yamask: {otherFormes: ['Yamask-Galar']},
   Zigzagoon: {otherFormes: ['Zigzagoon-Galar']},
@@ -7557,6 +7561,13 @@ const SS_PATCH: {[name: string]: DeepPartial<SpeciesData>} = {
     weightkg: 30,
     abilities: {0: 'Swift Swim'},
   },
+  'Blastoise-Gmax': {
+    types: ['Water'],
+    bs: {hp: 79, at: 83, df: 100, sa: 85, sd: 105, sp: 78},
+    weightkg: 0,
+    abilities: {0: 'Torrent'},
+    baseSpecies: 'Blastoise',
+  },
   Blipbug: {
     types: ['Bug'],
     bs: {hp: 25, at: 20, df: 20, sa: 25, sd: 45, sp: 45},
@@ -7617,6 +7628,14 @@ const SS_PATCH: {[name: string]: DeepPartial<SpeciesData>} = {
     bs: {hp: 80, at: 116, df: 75, sa: 65, sd: 75, sp: 119},
     weightkg: 33,
     abilities: {0: 'Blaze'},
+    otherFormes: ['Cinderace-Gmax'],
+  },
+  'Cinderace-Gmax': {
+    types: ['Fire'],
+    bs: {hp: 80, at: 116, df: 75, sa: 65, sd: 75, sp: 119},
+    weightkg: 0,
+    abilities: {0: 'Blaze'},
+    baseSpecies: 'Cinderace',
   },
   Clobbopus: {
     types: ['Fighting'],
@@ -8006,6 +8025,14 @@ const SS_PATCH: {[name: string]: DeepPartial<SpeciesData>} = {
     bs: {hp: 70, at: 85, df: 65, sa: 125, sd: 65, sp: 120},
     weightkg: 45.2,
     abilities: {0: 'Torrent'},
+    otherFormes: ['Inteleon-Gmax'],
+  },
+  'Inteleon-Gmax': {
+    types: ['Water'],
+    bs: {hp: 70, at: 85, df: 65, sa: 125, sd: 65, sp: 120},
+    weightkg: 0,
+    abilities: {0: 'Torent'},
+    baseSpecies: 'Inteleon',
   },
   'Kingler-Gmax': {
     types: ['Water'],
@@ -8013,6 +8040,12 @@ const SS_PATCH: {[name: string]: DeepPartial<SpeciesData>} = {
     weightkg: 0,
     abilities: {0: 'Hyper Cutter'},
     baseSpecies: 'Kingler',
+  },
+  'Kubfu': {
+    types: ["Fighting"],
+    bs: {hp: 60, at: 90, df: 60, sa: 53, sd: 50, sp: 72},
+    weightkg: 12,
+    abilities: {0: 'Inner Focus'},
   },
   'Lapras-Gmax': {
     types: ['Water', 'Ice'],
@@ -8142,6 +8175,13 @@ const SS_PATCH: {[name: string]: DeepPartial<SpeciesData>} = {
     abilities: {0: 'Static'},
     baseSpecies: 'Pikachu',
   },
+  'Pikachu-World': {
+    types: ['Electric'],
+    bs: {hp: 35, at: 55, df: 40, sa: 50, sd: 50, sp: 90},
+    weightkg: 6,
+    abilities: {0: 'Static'},
+    baseSpecies: 'Pikachu',
+  },
   Pincurchin: {
     types: ['Electric'],
     bs: {hp: 48, at: 101, df: 95, sa: 91, sd: 85, sp: 15},
@@ -8191,6 +8231,14 @@ const SS_PATCH: {[name: string]: DeepPartial<SpeciesData>} = {
     bs: {hp: 100, at: 125, df: 90, sa: 60, sd: 70, sp: 85},
     weightkg: 90,
     abilities: {0: 'Overgrow'},
+    otherFormes: ['Rillaboom-Gmax'],
+  },
+  'Rillaboom-Gmax': {
+    types: ['Grass'],
+    bs: {hp: 100, at: 125, df: 90, sa: 60, sd: 70, sp: 85},
+    weightkg: 80,
+    abilities: {0: 'Run Away'},
+    baseSpecies: 'Rillaboom',
   },
   Rolycoly: {
     types: ['Rock'],
@@ -8277,6 +8325,13 @@ const SS_PATCH: {[name: string]: DeepPartial<SpeciesData>} = {
     weightkg: 2.5,
     abilities: {0: 'Cheek Pouch'},
     nfe: true,
+  },
+  'Slowbro-Galar': {
+    types: ['Psychic', 'Poison'],
+    bs: {hp: 95, at: 100, df: 95, sa: 100, sd: 70, sp: 30},
+    weightkg: 70.5,
+    abilities: {0: 'Quick Draw'},
+    baseSpecies: 'Slowbro',
   },
   'Slowpoke-Galar': {
     types: ['Psychic'],
@@ -8367,6 +8422,41 @@ const SS_PATCH: {[name: string]: DeepPartial<SpeciesData>} = {
     abilities: {0: 'Punk Rock'},
     baseSpecies: 'Toxtricity',
   },
+  Urshifu: {
+    types: ['Fighting', 'Dark'],
+    bs: {hp: 75, at: 98, df: 70, sa: 114, sd: 70, sp: 75},
+    weightkg: 40,
+    abilities: {0: 'Punk Rock'},
+    otherFormes: ['Urshifu-Gmax', 'Urshifu-Rapid-Strike', 'Urshifu-Rapid-Strike-Gmax'],
+  },
+  'Urshifu-Rapid-Strike': {
+    types: ['Fighting', 'Water'],
+    bs: {hp: 100, at: 130, df: 100, sa: 63, sd: 60, sp: 97},
+    weightkg: 105,
+    abilities: {0: 'Unseen Fist'},
+    baseSpecies: 'Urshifu',
+  },
+  'Urshifu-Rapid-Strike-Gmax': {
+    types: ['Fighting', 'Water'],
+    bs: {hp: 100, at: 130, df: 100, sa: 63, sd: 60, sp: 97},
+    weightkg: 105,
+    abilities: {0: 'Unseen Fist'},
+    baseSpecies: 'Urshifu',
+  },
+  'Urshifu-Gmax': {
+    types: ['Fighting', 'Dark'],
+    bs: {hp: 100, at: 130, df: 100, sa: 63, sd: 60, sp: 97},
+    weightkg: 0,
+    abilities: {0: 'Unseen Fist'},
+    baseSpecies: 'Urshifu',
+  },
+  'Venusaur-Gmax': {
+    types: ['Poison', 'Fairy'],
+    bs: {hp: 80, at: 82, df: 83, sa: 100, sd: 100, sp: 80},
+    weightkg: 0,
+    abilities: {0: 'Levitate'},
+    baseSpecies: 'Venusaur',
+  },
   'Weezing-Galar': {
     types: ['Poison', 'Fairy'],
     bs: {hp: 65, at: 90, df: 120, sa: 85, sd: 70, sp: 60},
@@ -8426,6 +8516,22 @@ const SS_PATCH: {[name: string]: DeepPartial<SpeciesData>} = {
     weightkg: 785,
     abilities: {0: 'Dauntless Shield'},
     baseSpecies: 'Zamazenta',
+    gender: 'N',
+  },
+  Zarude: {
+    types: ['Dark', 'Grass'],
+    bs: {hp: 105, at: 120, df: 105, sa: 70, sd: 95, sp: 105},
+    weightkg: 70,
+    abilities: {0: 'Leaf Guard'},
+    gender: 'N',
+    otherFormes: ['Zarude-Dada'],
+  },
+  'Zarude-Dada': {
+    types: ['Dark', 'Grass'],
+    bs: {hp: 105, at: 120, df: 105, sa: 70, sd: 95, sp: 105},
+    weightkg: 70,
+    abilities: {0: 'Leaf Guard'},
+    baseSpecies: 'Zarude',
     gender: 'N',
   },
   'Zigzagoon-Galar': {

calc/src/mechanics/gen78.ts
@@ -87,7 +87,10 @@ export function calculateSMSS(
     return result;
   }
 
-  if (field.defenderSide.isProtected && !move.breaksProtect && !move.isZ && !attacker.isDynamaxed) {
+  const breaksProtect = move.breaksProtect || move.isZ || attacker.isDynamaxed
+  || (attacker.hasAbility('Unseen Fist') && move.flags.contact);
+
+  if (field.defenderSide.isProtected && !breaksProtect) {
     desc.isProtected = true;
     return result;
   }
@@ -160,6 +163,13 @@ export function calculateSMSS(
       move.type = 'Electric';
     } else if (attacker.named('Morpeko-Hangry')) {
       move.type = 'Dark';
+    } else if (move.named('Terrain Pulse')) {
+      move.type =
+      field.hasTerrain('Electric') ? 'Electric'
+      : field.hasTerrain('Grassy') ? 'Grass'
+      : field.hasTerrain('Misty') ? 'Fairy'
+      : field.hasTerrain('Psychic') ? 'Psychic'
+      : 'Normal';
     }
   }
 
@@ -361,8 +371,25 @@ export function calculateSMSS(
     basePower = r >= 4 ? 150 : r >= 3 ? 120 : r >= 2 ? 80 : r >= 1 ? 60 : 40;
     desc.moveBP = basePower;
     break;
+  case 'Expanding Force':
+    basePower = move.bp * ((isGrounded(attacker, field) && field.hasTerrain("Psychic")) ? 2 : 1);
+    move.target = (isGrounded(attacker, field) && field.hasTerrain("Psychic")) ? "allAdjacentFoes" : "normal";
+    desc.moveBP = basePower;
+    break;
+  case 'Misty Explosion':
+    basePower = move.bp * (field.hasTerrain("Misty") ? 2 : 1);
+    desc.moveBP = basePower;
+    break;
+  case 'Rising Voltage':
+    basePower = move.bp * (field.hasTerrain("Electric") ? 2 : 1);
+    desc.moveBP = basePower;
+    break;
   case 'Gyro Ball':
     basePower = Math.min(150, Math.floor((25 * defender.stats.spe) / attacker.stats.spe));
+    desc.moveBP = basePower;
+    break;
+  case 'Poltergeist':
+    basePower = move.bp * (!defender.hasItem ? 0 : 1);
     desc.moveBP = basePower;
     break;
   case 'Punishment':
@@ -401,6 +428,10 @@ export function calculateSMSS(
     basePower = move.bp * (field.isGravity ? 1.5 : 1);
     desc.moveBP = basePower;
     break;
+  case 'Steel Roller':
+    basePower = move.bp * (field.terrain ? 1 : 0);
+    desc.moveBP = basePower;
+    break;
   case 'Assurance':
     basePower = move.bp * (defender.hasAbility('Parental Bond (Child)') ? 2 : 1);
     // NOTE: desc.attackerAbility = 'Parental Bond' will already reflect this boost
@@ -412,6 +443,10 @@ export function calculateSMSS(
     break;
   case 'Weather Ball':
     basePower = field.weather && !field.hasWeather('Strong Winds') ? 100 : 50;
+    desc.moveBP = basePower;
+    break;
+  case 'Terrain Pulse':
+    basePower = field.terrain ? 100 : 50;
     desc.moveBP = basePower;
     break;
   case 'Fling':
@@ -438,6 +473,10 @@ export function calculateSMSS(
     break;
   case 'Water Shuriken':
     basePower = attacker.named('Greninja-Ash') && attacker.hasAbility('Battle Bond') ? 20 : 15;
+    desc.moveBP = basePower;
+    break;
+  case 'Lash Out':
+    basePower = move.bp * (countBoosts(gen, attacker.boosts) < 0 ? 2 : 1);
     desc.moveBP = basePower;
     break;
   case 'Crush Grip':

calc/src/data/abilities.ts
@@ -275,12 +275,14 @@ const SS = SM.concat([
   'Power Spot',
   'Propeller Tail',
   'Punk Rock',
+  'Quick Draw',
   'Ripen',
   'Sand Spit',
   'Screen Cleaner',
   'Stalwart',
   'Steam Engine',
   'Steely Spirit',
+  'Unseen Fist',
   'Wandering Spirit',
 ]);
 

commit 0f8d9343b48c8a7c914ac1968d2446720c2c24a4

Fix build errors introduced by #397 (#398)

calc/src/data/moves.ts
@@ -2893,10 +2893,12 @@ const SS_PATCH: {[name: string]: DeepPartial<MoveData>} = {
   },
   'Burning Jealousy': {
     bp: 70,
-    type: 'Dragon',
+    type: 'Fire',
     target: 'allAdjacentFoes',
     category: 'Special',
     secondaries: true,
+    zp: 140,
+    maxPower: 120,
   },
   'Clangorous Soul': {bp: 0, type: 'Dragon', isSound: true},
   'Coaching': {
@@ -2908,6 +2910,7 @@ const SS_PATCH: {[name: string]: DeepPartial<MoveData>} = {
     bp: 0,
     type: 'Poison',
     category: 'Status',
+    target: 'allAdjacent',
   },
   'Court Change': {bp: 0, type: 'Normal'},
   Decorate: {bp: 0, type: 'Fairy'},
@@ -2933,6 +2936,7 @@ const SS_PATCH: {[name: string]: DeepPartial<MoveData>} = {
     category: 'Physical',
     makesContact: true,
     multihit: 2,
+    zp: 100,
     maxPower: 130,
   },
   'Dynamax Cannon': {
@@ -2953,6 +2957,8 @@ const SS_PATCH: {[name: string]: DeepPartial<MoveData>} = {
     bp: 80,
     type: 'Psychic',
     category: 'Special',
+    zp: 160,
+    maxPower: 130,
   },
   'False Surrender': {
     bp: 80,
@@ -2975,6 +2981,9 @@ const SS_PATCH: {[name: string]: DeepPartial<MoveData>} = {
     bp: 60,
     type: 'Water',
     category: 'Physical',
+    makesContact: true,
+    zp: 120,
+    maxPower: 110,
   },
   'G-Max Befuddle': {
     bp: 10,
@@ -3212,8 +3221,8 @@ const SS_PATCH: {[name: string]: DeepPartial<MoveData>} = {
     type: 'Grass',
     category: 'Physical',
     makesContact: true,
-    zp: 160,
-    maxPower: 130,
+    zp: 140,
+    maxPower: 120,
   },
   'Grav Apple': {
     bp: 80,
@@ -3242,6 +3251,8 @@ const SS_PATCH: {[name: string]: DeepPartial<MoveData>} = {
     type: 'Dark',
     makesContact: true,
     category: 'Physical',
+    zp: 140,
+    maxPower: 130,
   },
   'Life Dew': {bp: 0, type: 'Water'},
   'Magic Powder': {bp: 0, type: 'Psychic'},
@@ -3383,11 +3394,16 @@ const SS_PATCH: {[name: string]: DeepPartial<MoveData>} = {
     bp: 120,
     type: 'Rock',
     category: 'Special',
+    zp: 190,
+    maxPower: 140,
   },
   'Misty Explosion': {
     bp: 100,
     type: 'Fairy',
     category: 'Special',
+    target: 'allAdjacent',
+    zp: 180,
+    maxPower: 130,
   },
   'No Retreat': {bp: 0, type: 'Fighting'},
   Obstruct: {bp: 0, type: 'Dark', priority: 4},
@@ -3402,9 +3418,11 @@ const SS_PATCH: {[name: string]: DeepPartial<MoveData>} = {
     maxPower: 130,
   },
   Poltergeist: {
-    bp: 80,
-    type: 'Dark',
+    bp: 110,
+    type: 'Ghost',
     category: 'Physical',
+    zp: 185,
+    maxPower: 140,
   },
   'Pyro Ball': {
     bp: 120,
@@ -3419,14 +3437,24 @@ const SS_PATCH: {[name: string]: DeepPartial<MoveData>} = {
     bp: 70,
     type: 'Electric',
     category: 'Special',
+    zp: 140,
+    maxPower: 120,
   },
   'Scale Shot': {
     bp: 25,
     type: 'Dragon',
     category: 'Physical',
-    multihit: 5,
+    multihit: [2, 5],
     zp: 140,
     maxPower: 130,
+  },
+  'Shell Side Arm': {
+    bp: 90,
+    type: 'Poison',
+    category: 'Special',
+    secondaries: true,
+    zp: 175,
+    maxPower: 90,
   },
   'Snap Trap': {
     bp: 35,
@@ -3448,6 +3476,8 @@ const SS_PATCH: {[name: string]: DeepPartial<MoveData>} = {
     type: 'Ground',
     category: 'Special',
     secondaries: true,
+    zp: 140,
+    maxPower: 120,
   },
   'Skitter Smack': {
     bp: 70,
@@ -3455,6 +3485,8 @@ const SS_PATCH: {[name: string]: DeepPartial<MoveData>} = {
     category: 'Physical',
     makesContact: true,
     secondaries: true,
+    zp: 140,
+    maxPower: 120,
   },
   'Spirit Break': {
     bp: 75,
@@ -3477,6 +3509,9 @@ const SS_PATCH: {[name: string]: DeepPartial<MoveData>} = {
     bp: 130,
     type: 'Steel',
     category: 'Physical',
+    makesContact: true,
+    zp: 195,
+    maxPower: 140,
   },
   'Strange Steam': {
     bp: 90,
@@ -3493,6 +3528,8 @@ const SS_PATCH: {[name: string]: DeepPartial<MoveData>} = {
     makesContact: true,
     willCrit: true,
     multihit: 3,
+    zp: 100,
+    maxPower: 90,
   },
   'Terrain Pulse': {
     bp: 50,
@@ -3508,7 +3545,8 @@ const SS_PATCH: {[name: string]: DeepPartial<MoveData>} = {
     category: 'Physical',
     makesContact: true,
     multihit: 3,
-    maxPower: 130,
+    zp: 120,
+    maxPower: 140,
   },
   'Wicked Blow': {
     bp: 80,
@@ -3516,6 +3554,8 @@ const SS_PATCH: {[name: string]: DeepPartial<MoveData>} = {
     category: 'Physical',
     makesContact: true,
     willCrit: true,
+    zp: 160,
+    maxPower: 130,
   },
   'Stuff Cheeks': {bp: 0, type: 'Normal'},
   'Tar Shot': {bp: 0, type: 'Rock'},

calc/src/data/species.ts
@@ -8031,7 +8031,7 @@ const SS_PATCH: {[name: string]: DeepPartial<SpeciesData>} = {
     types: ['Water'],
     bs: {hp: 70, at: 85, df: 65, sa: 125, sd: 65, sp: 120},
     weightkg: 0,
-    abilities: {0: 'Torent'},
+    abilities: {0: 'Torrent'},
     baseSpecies: 'Inteleon',
   },
   'Kingler-Gmax': {
@@ -8042,9 +8042,10 @@ const SS_PATCH: {[name: string]: DeepPartial<SpeciesData>} = {
     baseSpecies: 'Kingler',
   },
   'Kubfu': {
-    types: ["Fighting"],
-    bs: {hp: 60, at: 90, df: 60, sa: 53, sd: 50, sp: 72},
+    types: ['Fighting'],
+    bs: {hp: 60, at: 90, df: 60, sa: 53, sd: 50, sp: 72}, // FIXME should be 72
     weightkg: 12,
+    nfe: true,
     abilities: {0: 'Inner Focus'},
   },
   'Lapras-Gmax': {
@@ -8061,6 +8062,15 @@ const SS_PATCH: {[name: string]: DeepPartial<SpeciesData>} = {
     abilities: {0: 'Pickup'},
     nfe: true,
     baseSpecies: 'Linoone',
+  },
+  Magearna: {otherFormes: ['Magearna-Original']},
+  'Magearna-Original': {
+    baseSpecies: 'Magearna',
+    types: ['Steel', 'Fairy'],
+    bs: {hp: 80, at: 95, df: 115, sa: 130, sd: 115, sp: 65},
+    weightkg: 80.5,
+    gender: 'N',
+    abilities: {0: 'Soul-Heart'},
   },
   'Machamp-Gmax': {
     types: ['Fighting'],
@@ -8236,8 +8246,8 @@ const SS_PATCH: {[name: string]: DeepPartial<SpeciesData>} = {
   'Rillaboom-Gmax': {
     types: ['Grass'],
     bs: {hp: 100, at: 125, df: 90, sa: 60, sd: 70, sp: 85},
-    weightkg: 80,
-    abilities: {0: 'Run Away'},
+    weightkg: 0,
+    abilities: {0: 'Overgrow'},
     baseSpecies: 'Rillaboom',
   },
   Rolycoly: {
@@ -8337,6 +8347,7 @@ const SS_PATCH: {[name: string]: DeepPartial<SpeciesData>} = {
     types: ['Psychic'],
     bs: {hp: 90, at: 65, df: 65, sa: 40, sd: 40, sp: 15},
     weightkg: 36,
+    nfe: true,
     abilities: {0: 'Gluttony'},
     baseSpecies: 'Slowpoke',
   },
@@ -8424,9 +8435,9 @@ const SS_PATCH: {[name: string]: DeepPartial<SpeciesData>} = {
   },
   Urshifu: {
     types: ['Fighting', 'Dark'],
-    bs: {hp: 75, at: 98, df: 70, sa: 114, sd: 70, sp: 75},
-    weightkg: 40,
-    abilities: {0: 'Punk Rock'},
+    bs: {hp: 100, at: 130, df: 100, sa: 63, sd: 60, sp: 97},
+    weightkg: 105,
+    abilities: {0: 'Unseen Fist'},
     otherFormes: ['Urshifu-Gmax', 'Urshifu-Rapid-Strike', 'Urshifu-Rapid-Strike-Gmax'],
   },
   'Urshifu-Rapid-Strike': {
@@ -8451,10 +8462,10 @@ const SS_PATCH: {[name: string]: DeepPartial<SpeciesData>} = {
     baseSpecies: 'Urshifu',
   },
   'Venusaur-Gmax': {
-    types: ['Poison', 'Fairy'],
+    types: ['Grass', 'Poison'],
     bs: {hp: 80, at: 82, df: 83, sa: 100, sd: 100, sp: 80},
     weightkg: 0,
-    abilities: {0: 'Levitate'},
+    abilities: {0: 'Overgrow'},
     baseSpecies: 'Venusaur',
   },
   'Weezing-Galar': {
@@ -8518,22 +8529,23 @@ const SS_PATCH: {[name: string]: DeepPartial<SpeciesData>} = {
     baseSpecies: 'Zamazenta',
     gender: 'N',
   },
-  Zarude: {
-    types: ['Dark', 'Grass'],
-    bs: {hp: 105, at: 120, df: 105, sa: 70, sd: 95, sp: 105},
-    weightkg: 70,
-    abilities: {0: 'Leaf Guard'},
-    gender: 'N',
-    otherFormes: ['Zarude-Dada'],
-  },
-  'Zarude-Dada': {
-    types: ['Dark', 'Grass'],
-    bs: {hp: 105, at: 120, df: 105, sa: 70, sd: 95, sp: 105},
-    weightkg: 70,
-    abilities: {0: 'Leaf Guard'},
-    baseSpecies: 'Zarude',
-    gender: 'N',
-  },
+  // Unreleased
+  // Zarude: {
+  //   types: ['Dark', 'Grass'],
+  //   bs: {hp: 105, at: 120, df: 105, sa: 70, sd: 95, sp: 105},
+  //   weightkg: 70,
+  //   abilities: {0: 'Leaf Guard'},
+  //   gender: 'N',
+  //   otherFormes: ['Zarude-Dada'],
+  // },
+  // 'Zarude-Dada': {
+  //   types: ['Dark', 'Grass'],
+  //   bs: {hp: 105, at: 120, df: 105, sa: 70, sd: 95, sp: 105},
+  //   weightkg: 70,
+  //   abilities: {0: 'Leaf Guard'},
+  //   baseSpecies: 'Zarude',
+  //   gender: 'N',
+  // },
   'Zigzagoon-Galar': {
     types: ['Dark', 'Normal'],
     bs: {hp: 38, at: 30, df: 41, sa: 30, sd: 41, sp: 60},

calc/src/mechanics/gen78.ts
@@ -87,8 +87,8 @@ export function calculateSMSS(
     return result;
   }
 
-  const breaksProtect = move.breaksProtect || move.isZ || attacker.isDynamaxed
-  || (attacker.hasAbility('Unseen Fist') && move.flags.contact);
+  const breaksProtect = move.breaksProtect || move.isZ || attacker.isDynamaxed ||
+  (attacker.hasAbility('Unseen Fist') && move.flags.contact);
 
   if (field.defenderSide.isProtected && !breaksProtect) {
     desc.isProtected = true;
@@ -372,16 +372,17 @@ export function calculateSMSS(
     desc.moveBP = basePower;
     break;
   case 'Expanding Force':
-    basePower = move.bp * ((isGrounded(attacker, field) && field.hasTerrain("Psychic")) ? 2 : 1);
-    move.target = (isGrounded(attacker, field) && field.hasTerrain("Psychic")) ? "allAdjacentFoes" : "normal";
+    basePower = move.bp * ((isGrounded(attacker, field) && field.hasTerrain('Psychic')) ? 2 : 1);
+    move.target =
+      (isGrounded(attacker, field) && field.hasTerrain('Psychic')) ? 'allAdjacentFoes' : 'normal';
     desc.moveBP = basePower;
     break;
   case 'Misty Explosion':
-    basePower = move.bp * (field.hasTerrain("Misty") ? 2 : 1);
+    basePower = move.bp * (field.hasTerrain('Misty') ? 2 : 1);
     desc.moveBP = basePower;
     break;
   case 'Rising Voltage':
-    basePower = move.bp * (field.hasTerrain("Electric") ? 2 : 1);
+    basePower = move.bp * (field.hasTerrain('Electric') ? 2 : 1);
     desc.moveBP = basePower;
     break;
   case 'Gyro Ball':

calc/src/test/data.test.ts
@@ -9,6 +9,7 @@ const pkmn = {Generations: new Generations(Dex)};
 
 const gens = [1, 2, 3, 4, 5, 6, 7, 8] as I.GenerationNum[];
 
+// TODO: reverse expectations everywhere - p should expected and c should be received!
 describe('Generations', () => {
   it('abilities', () => {
     for (const gen of gens) {
@@ -48,6 +49,9 @@ describe('Generations', () => {
 
       expect(p).toHaveLength(c.size);
       for (const move of p) {
+        // FIXME: workaround for incorrect PS data snapshotted by @pkmn/dex v0.0.8
+        if (move.id === 'terrainpulse') move.flags.pulse = 1;
+
         expect(move).toEqual(c.get(move.id));
         c.delete(move.id);
       }
@@ -60,9 +64,11 @@ describe('Generations', () => {
       const p = Array.from(pkmn.Generations.get(gen).species);
       const c = new Map<I.ID, I.Specie>();
       for (const specie of calc.Generations.get(gen).species) c.set(specie.id, specie);
-
       expect(p).toHaveLength(c.size);
       for (const specie of p) {
+        // FIXME: workaround for incorrect PS data snapshotted by @pkmn/dex v0.0.8
+        if (specie.id === 'kubfu') (specie.baseStats as I.StatsTable).spe = 72;
+
         expect(specie).toEqual(c.get(specie.id));
         c.delete(specie.id);
       }